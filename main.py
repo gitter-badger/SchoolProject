2076e51640d87b3915fdfca0b317eed11fbaeddf
MSF Jarvis <msfjarvis@gmail.com>
2016-04-22 16:39:01 +0530

    Initial Commit
    Signed-off-by: Harsh Shandilya <msfjarvis@gmail.com>

---
+++ main.py
@@ -0,0 +1,10 @@
+class Library():
+    def __init__(self):
+        self.libraryname = ''
+        self.booknum = 0
+        self.books = []
+        self.issuers = {}
+        self.booksissued = []
+
+
+lib1 = Library()
