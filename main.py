49f7b508d103c0e6abf07d8a308b835079792418
Harsh Shandilya <msfjarvis@gmail.com>
2016-04-25 01:26:54 +0530

    Add comments and fix string
    Signed-off-by: Harsh Shandilya <msfjarvis@gmail.com>

--- main.py
+++ main.py
@@ -1,32 +1,32 @@
-from time import sleep
-import json
+from time import sleep	#for waiting times between functions
+import json				#for backend
 
-def load_config(__file):
+def load_config(__file):		#Loads the data and
     __local_config = False
     try:
         with open(__file) as f:
             __local_config = json.load(f)
-    except Exception as e:
+    except Exception as e:		#Catches exceptions too!
         print (e)
     return __local_config
 
-def save_config(__obj, __file):
+def save_config(__obj, __file):		#Writes __obj to __file
     try:
         with open(__file, 'w') as f:
             json.dump(__obj, f, indent=2)
-    except Exception as e:
+    except Exception as e:			#With more exception handling, just in case
         print (e)
         return False
     return True
 
-def stop():
+def stop():							#The function we (will) call to end the program. This is extremely limited for now, and needs to be rewritten
     global config
     if save_config(config, 'config.json') == False:
         print ("error saving config! any changes will not be saved!")
     quit()
 
-class Library():
-    def __init__(self):
+class Library():					#The main class
+    def __init__(self):				#The variables get initialised here
         self.libraryname = ''
         self.booknum = 0
         self.books = []
@@ -35,9 +35,9 @@ class Library():
         print 'Library initialised...'
         sleep(1)
         print 'Please add required data now...'
-        self.readdata()
+        self.readdata()				#Let's add some values to the variables
 
-    def readdata(self):
+    def readdata(self):				#As it's name says, reads the data into our variables
         self.libraryname = raw_input("Enter name of the Library...")
         self.booknum = int(raw_input("Enter total number of books associated with the Library..."))
         filename1 = raw_input("Enter filename to load book names from (default := books.json )...")
@@ -48,7 +48,7 @@ class Library():
             print "No books present in config! This is an error..."
             sleep(0.5)
             quit()
-        filename2 = raw_input("Enter filename for issuers dictionary of format "name":[book1,book2,...] default filename is issuers.json...)
+        filename2 = raw_input("Enter filename for issuers dictionary of format \"name\":[book1,book2,...] default filename is issuers.json...)"
         if filename2 == '' :
             filename2 = 'issuers.json'
         self.issuers = load_config(filename2)
